b6e4719e9ae82c5da5a139f9b52841615711029a
RajitPRAJAPATI <rajitkumarprajapati7@gmail.com>
2026-05-06 17:26:19 +0530

    Task Manager

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
 const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
